e7fafe92c0adb861fc1c77baec529ceccaf6e1d5
Andrew Christensen <andrew.christensen014@gmail.com>
2020-03-03 02:37:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string) {
   } else if (string === string.toLowerCase()) {
     return "I can't hear you!"
   } else if (strinng === "I love you, Grandma.") {
-    return "I love you, too"
+    return "I love you, too."
   } else {}
 }
